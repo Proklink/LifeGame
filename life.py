d4cf225c5a95cfb5c9141c6989524f6980991f0f
Ogorodnikov Daniil <danil.ogorodnikov.2000@mail.ru>
2023-02-09 20:22:06 +0300

    full lifegame

--- life.py
+++ life.py
@@ -1,7 +1,7 @@
 import pygame
 import random, time, math
 
-WIDTH = 360
+WIDTH = 360 *2
 HEIGHT = 480
 FPS = 20
 
@@ -17,169 +17,69 @@ RP = (255, 0, 128)
 ASD = (188, 196, 96)
 ASD1 = (188, 0, 96)
 
-colors_live = [WHITE]
-colors_dead = [BLACK]
+colors_live = [WHITE, RED]
+colors_dead = [BLACK, GREEN]
 
 rect_width = 10
 rect_height = 10
 
-W = int(WIDTH / rect_width)
-H = int(HEIGHT / rect_height)
+W = int(WIDTH / rect_width) #36
+H = int(HEIGHT / rect_height) #48
 
 screen = []
 random.seed(int(time.time()))
-for x in range(W):
+
+def init():
     for y in range(H):
-        rect = pygame.Rect(x * rect_width + 2, y * rect_height + 2, rect_width, rect_height)
-        mas = [0, rect]
-        
-        r = random.randint(1, 2000)
-        _x = random.randint(0, 1)
-        _y = not _x
-        if r > random.randint(1, r) and r < random.randint(r, 2000):
-            if x % 2 == _x and y % 2 == _y:
-                mas[0] = random.randint(0, 1)
-        screen.append(mas)
+        for x in range(W):
+            rect = pygame.Rect(x * rect_width, y * rect_height, rect_width, rect_height)
+            mas = [0, rect]
+            
+            r = random.randint(1, 2000)
+            _x = random.randint(0, 1)
+            _y = not _x
+            if r > random.randint(1, r) and r < random.randint(r, 2000):
+                if x % 2 == _x and y % 2 == _y:
+                    mas[0] = random.randint(0, 1)
+            screen.append(mas)
 
 
 def pixel(x, y):
-    return screen[x * W + y][1]
+    return screen[y * W + x][1]
 
 def value(x, y):
-    return screen[x * W + y][0]
+    if x < 0 or y < 0 or x >= W or y >= H:
+        return 0
+
+    return screen[y * W + x][0]
 
 def set_value(x, y, val):
-    screen[x * W + y][0] = val
+    screen[y * W + x][0] = val
 
 def dead(x, y):
-    if x == 0 and y == 0:
-        if value(x + 1, y) + value(x + 1, y + 1) + value(x, y + 1) == 3:
-            set_value(x, y, 1)
-
-    elif x == 0 and y == H - 1:
-        if value(x - 1, y) + value(x - 1, y + 1) + value(x, y + 1) == 3:
-            set_value(x, y, 1)
-
-    elif x == W - 1 and y == 0:
-        if value(x, y - 1) + value(x + 1, y - 1) + value(x + 1, y) == 3:
-            set_value(x, y, 1)
-
-    elif x == W - 1 and y == H - 1:
-        if value(x - 1, y) + value(x - 1, y - 1) + value(x, y - 1) == 3:
-            set_value(x, y, 1)
-
-    elif x == 0 and y > 0 and y < H - 1:
-        if value(x, y + 1) + \
-                value(x, y - 1) + \
-                value(x + 1, y - 1) + \
-                value(x + 1, y) + \
-                value(x + 1, y + 1) == 3:
-            set_value(x, y, 1)
-
-    
-    elif x == W - 1 and y > 0 and y < H - 1:
-        if value(x, y + 1) + \
-                value(x, y - 1) + \
-                value(x - 1, y - 1) + \
-                value(x - 1, y) + \
-                value(x - 1, y + 1) == 3:
-            set_value(x, y, 1)
-
-    elif x > 0 and x < W - 1 and y == 0:
-        if value(x - 1, y) + \
-                value(x + 1, y) + \
-                value(x - 1, y + 1) + \
-                value(x, y + 1) + \
-                value(x + 1, y + 1) == 3:
-            set_value(x, y, 1)
-
-    elif x > 0 and x < W - 1 and y == H - 1:
-        if value(x - 1, y) + \
-                value(x + 1, y) + \
-                value(x - 1, y - 1) + \
-                value(x, y - 1) + \
-                value(x + 1, y - 1) == 3:
-            set_value(x, y, 1)
-
-    else:
-        if value(x - 1, y - 1) + \
-                value(x - 1, y) + \
-                value(x - 1, y + 1) + \
-                                    \
-                value(x, y - 1) + \
-                value(x, y + 1) + \
-                                    \
-                value(x + 1, y - 1) + \
-                value(x + 1, y) + \
-                value(x + 1, y + 1) == 3:
-            set_value(x, y, 1)
+    if value(x - 1, y - 1) + value(x - 1, y) + value(x - 1, y + 1) + \
+       value(x, y - 1) + value(x, y + 1) + value(x + 1, y - 1) + \
+       value(x + 1, y) + value(x + 1, y + 1) == 3:
+        set_value(x, y, 1)
 
 def alive(x, y):
-    if x == 0 and y == 0:
-        val = value(x + 1, y) + value(x + 1, y + 1) + value(x, y + 1)
-
-    elif x == 0 and y == H - 1:
-        val = value(x - 1, y) + value(x - 1, y + 1) + value(x, y + 1)
-
-    elif x == W - 1 and y == 0:
-        val = value(x, y - 1) + value(x + 1, y - 1) + value(x + 1, y)
-
-    elif x == W - 1 and y == H - 1:
-        val = value(x - 1, y) + value(x - 1, y - 1) + value(x, y - 1)
-
-    elif x == 0 and y > 0 and y < H - 1:
-        val = value(x, y + 1) + \
-                value(x, y - 1) + \
-                value(x + 1, y - 1) + \
-                value(x + 1, y) + \
-                value(x + 1, y + 1)
-
-    elif x == W - 1 and y > 0 and y < H - 1:
-        val = value(x, y + 1) + \
-                value(x, y - 1) + \
-                value(x - 1, y - 1) + \
-                value(x - 1, y) + \
-                value(x - 1, y + 1)
-
-    elif x > 0 and x < W - 1 and y == 0:
-        val = value(x - 1, y) + \
-                value(x + 1, y) + \
-                value(x - 1, y + 1) + \
-                value(x, y + 1) + \
-                value(x + 1, y + 1)
-
-    elif x > 0 and x < W - 1 and y == H - 1:
-        val = value(x - 1, y) + \
-                value(x + 1, y) + \
-                value(x - 1, y - 1) + \
-                value(x, y - 1) + \
-                value(x + 1, y - 1)
-
-    else:
-        val = value(x - 1, y - 1) + \
-                value(x - 1, y) + \
-                value(x - 1, y + 1) + \
-                                    \
-                value(x, y - 1) + \
-                value(x, y + 1) + \
-                                    \
-                value(x + 1, y - 1) + \
-                value(x + 1, y) + \
-                value(x + 1, y + 1)
+    val = value(x - 1, y - 1) + value(x - 1, y) + value(x - 1, y + 1) + \
+          value(x, y - 1) + value(x, y + 1) + value(x + 1, y - 1) + \
+          value(x + 1, y) + value(x + 1, y + 1)
     if val < 2 or val > 3:
         set_value(x, y, 0)
 
 def update():
-    for x in range(W):
-        for y in range(H):
+    for y in range(H):
+        for x in range(W):
             if value(x, y) == 0:
                 dead(x, y)
             else:
                 alive(x, y)
             
 def render(window):
-    for x in range(W):
-        for y in range(H):
+    for y in range(H):
+        for x in range(W):
             if value(x, y) == 0:
                 window.fill(colors_dead[random.randint(0, len(colors_dead) - 1)], pixel(x, y))
             else:
@@ -188,6 +88,7 @@ def render(window):
 # Создаем игру и окно
 pygame.init()
 pygame.mixer.init()
+init()
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
